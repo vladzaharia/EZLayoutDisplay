91574de82de2f8611f2fb2b96e474520737728b2
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-27 18:19:02 -0400

    Add the SupportedKeyboardModel property signature

--- src/plugins/InvvardDev.EZLayoutDisplay.PluginContract/IKeyboardContract.cs
+++ src/plugins/InvvardDev.EZLayoutDisplay.PluginContract/IKeyboardContract.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace InvvardDev.EZLayoutDisplay.PluginContract
 {
     public interface IKeyboardContract
     {
-
+        /// <summary>
+        /// Gets or sets the list of supported keyboard model.
+        /// </summary>
+        IEnumerable<string> SupportedKeyboardModel { get; }
     }
 }
