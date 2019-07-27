037747bd5d029bc24c26fee58bdeba1e8227f5a8
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-27 17:29:34 -0400

    Remove ergoDoxEzLayoutDefinition

--- src/InvvardDev.EZLayoutDisplay.Desktop/Properties/Resources.Designer.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -66,16 +66,6 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Properties {
         internal static byte[] keyDefinitions {
             get {
                 object obj = ResourceManager.GetObject("keyDefinitions", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] layoutDefinition {
-            get {
-                object obj = ResourceManager.GetObject("layoutDefinition", resourceCulture);
                 return ((byte[])(obj));
             }
         }
