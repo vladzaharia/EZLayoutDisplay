5b50251ab0f252e55ffa64641308d2f3af2b05d6
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-27 18:19:33 -0400

    Implement the SupportedKeyboardModel property

--- src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ErgoDoxEz/ErgoDoxEzKeyboard.cs
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ErgoDoxEz/ErgoDoxEzKeyboard.cs
@@ -1,8 +1,17 @@
-﻿using InvvardDev.EZLayoutDisplay.PluginContract;
+﻿using System.Collections.Generic;
+using InvvardDev.EZLayoutDisplay.PluginContract;
 
 namespace InvvardDev.EZLayoutDisplay.Plugin.ErgoDoxEz
 {
     public class ErgoDoxEzKeyboard : IKeyboardContract
     {
+        public IEnumerable<string> SupportedKeyboardModel { get; }
+
+        public ErgoDoxEzKeyboard()
+        {
+            SupportedKeyboardModel = new List<string> {
+                                                          "ergodox ez"
+                                                      };
+        }
     }
 }
