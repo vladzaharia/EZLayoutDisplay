06b99dd20a5062397a273615fbc2ecbac16cf12b
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-28 22:29:14 -0400

    Add ILayoutService and implementation

---
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.CommonZsa/Service/ILayoutService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using InvvardDev.EZLayoutDisplay.Desktop.Model;
+
+namespace InvvardDev.EZLayoutDisplay.Plugin.CommonZsa.Service
+{
+    internal interface ILayoutService
+    {
+        /// <summary>
+        /// Gets the list of <see cref="KeyTemplate"/> from the local repository.
+        /// </summary>
+        /// <param name="filePath">The file path to load the list of <see cref="KeyTemplate"/> from.</param>
+        /// <returns>An <see cref="IEnumerable{KeyTemplate}"/></returns>
+        Task<IEnumerable<KeyTemplate>> GetLayoutTemplate(string filePath);
+    }
+}
---
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.CommonZsa/Service/LayoutService.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using InvvardDev.EZLayoutDisplay.Desktop.Model;
+using InvvardDev.EZLayoutDisplay.Desktop.Properties;
+using Newtonsoft.Json;
+
+namespace InvvardDev.EZLayoutDisplay.Plugin.CommonZsa.Service
+{
+    public class LayoutService : ILayoutService
+    {
+        /// <inheritdoc />
+        public async Task<IEnumerable<KeyTemplate>> GetLayoutTemplate(string filePath)
+        {
+            var fileContent = GetFileContent(filePath);
+
+            IEnumerable<KeyTemplate> layoutTemplate = await ReadLayoutDefinition(fileContent);
+
+            return layoutTemplate;
+        }
+
+        private byte[] GetFileContent(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"File {filePath} is missing. Install the plugin again to fix.");
+            }
+
+            var fileContent = File.ReadAllBytes(filePath);
+
+            return fileContent;
+        }
+
+        private async Task<IEnumerable<KeyTemplate>> ReadLayoutDefinition(byte[] fileContent)
+        {
+            if (fileContent.Length <= 0)
+            {
+                return new List<KeyTemplate>();
+            }
+
+            var layoutTemplate = await Task.Run(() => {
+                                                    var json = Encoding.Default.GetString(fileContent);
+
+                                                    var layoutDefinition = JsonConvert.DeserializeObject<IEnumerable<KeyTemplate>>(json);
+
+                                                    return layoutDefinition;
+                                                });
+
+            return layoutTemplate;
+        }
+
+    }
+}
