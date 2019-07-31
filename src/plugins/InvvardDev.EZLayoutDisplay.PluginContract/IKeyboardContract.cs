6b9d42bf56d9bd084a43f993f4cf1058605bd148
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-30 22:36:10 -0400

    Add new method signatures

--- src/plugins/InvvardDev.EZLayoutDisplay.PluginContract/IKeyboardContract.cs
+++ src/plugins/InvvardDev.EZLayoutDisplay.PluginContract/IKeyboardContract.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using InvvardDev.EZLayoutDisplay.PluginContract.Model;
 
 namespace InvvardDev.EZLayoutDisplay.PluginContract
 {
@@ -10,11 +12,22 @@ namespace InvvardDev.EZLayoutDisplay.PluginContract
         IEnumerable<string> SupportedKeyboardModel { get; }
 
         /// <summary>
+        /// Loads the submitted <see cref="EZLayout"/>.
+        /// </summary>
+        /// <param name="ezLayout">The <see cref="EZLayout"/> object to load.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        Task LoadLayoutAsync(EZLayout ezLayout);
+
+        /// <summary>
         /// Gets the current layer name.
         /// </summary>
         /// <returns>The current layer name.</returns>
         string GetCurrentLayerName();
 
-
+        /// <summary>
+        /// Gets the keyboard view.
+        /// </summary>
+        /// <returns>A rendered Keyboard user control.</returns>
+        object GetKeyboardView();
     }
 }
