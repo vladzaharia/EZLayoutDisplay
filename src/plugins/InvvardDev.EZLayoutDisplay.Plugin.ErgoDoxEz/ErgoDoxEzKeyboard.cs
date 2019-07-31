abf74883002f7d3fe9b2122d14612e909f6f0390
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-30 22:36:46 -0400

    Implement the new ILayoutService methods

--- src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ErgoDoxEz/ErgoDoxEzKeyboard.cs
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ErgoDoxEz/ErgoDoxEzKeyboard.cs
@@ -1,22 +1,47 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using InvvardDev.EZLayoutDisplay.Plugin.CommonZsa.Service;
+using InvvardDev.EZLayoutDisplay.Plugin.ErgoDoxEz.View;
+using InvvardDev.EZLayoutDisplay.Plugin.ErgoDoxEz.ViewModel;
 using InvvardDev.EZLayoutDisplay.PluginContract;
+using InvvardDev.EZLayoutDisplay.PluginContract.Model;
 
 namespace InvvardDev.EZLayoutDisplay.Plugin.ErgoDoxEz
 {
     public class ErgoDoxEzKeyboard : IKeyboardContract
     {
+        private const string LayoutDefinitionPath = "/data/ergoDoxEzLayoutDefinition.json";
+        private readonly ILayoutService _layoutService;
+        private ErgoDoxEzViewModel _viewModel;
+
         public IEnumerable<string> SupportedKeyboardModel { get; }
 
         public ErgoDoxEzKeyboard()
         {
+            _layoutService = new LayoutService();
             SupportedKeyboardModel = new List<string> {
                                                           "ergodox ez"
                                                       };
         }
 
+        public async Task LoadLayoutAsync(EZLayout ezLayout)
+        {
+            var layoutDefinition = await _layoutService.LoadLayoutDefinitionAsync(LayoutDefinitionPath);
+            var layoutTemplates = await _layoutService.PopulateLayoutTemplatesAsync(layoutDefinition, ezLayout);
+            _viewModel = new ErgoDoxEzViewModel(layoutTemplates);
+        }
+
         public string GetCurrentLayerName()
         {
             return "Not implemented yet";
+        }
+
+        public object GetKeyboardView()
+        {
+            return new ErgoDoxEzView {
+                                         DataContext = _viewModel
+                                     };
         }
     }
 }
