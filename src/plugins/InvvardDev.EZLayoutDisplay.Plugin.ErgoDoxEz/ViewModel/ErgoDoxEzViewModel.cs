1c73ef11e043fa2749e21806f4c5ea2770f76728
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-30 22:37:16 -0400

    Add base view model code

--- src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ErgoDoxEz/ViewModel/ErgoDoxEzViewModel.cs
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ErgoDoxEz/ViewModel/ErgoDoxEzViewModel.cs
@@ -1,7 +1,41 @@
-﻿namespace InvvardDev.EZLayoutDisplay.Plugin.ErgoDoxEz.ViewModel
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using InvvardDev.EZLayoutDisplay.PluginContract.Model;
+
+namespace InvvardDev.EZLayoutDisplay.Plugin.ErgoDoxEz.ViewModel
 {
-    public class ErgoDoxEzViewModel
+    public class ErgoDoxEzViewModel : ViewModelBase
     {
-        
+        #region Fields
+
+        private IEnumerable<IEnumerable<KeyTemplate>> _layoutTemplates;
+        private ObservableCollection<KeyTemplate> _currentLayoutTemplate;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the layout template.
+        /// </summary>
+        internal ObservableCollection<KeyTemplate> CurrentLayoutTemplate
+        {
+            get => _currentLayoutTemplate;
+            set => Set(ref _currentLayoutTemplate, value);
+        }
+
+        #endregion
+
+        public ErgoDoxEzViewModel(IEnumerable<IEnumerable<KeyTemplate>> layoutTemplates)
+        {
+            _layoutTemplates = layoutTemplates;
+            CurrentLayoutTemplate = new ObservableCollection<KeyTemplate>();
+        }
+
+        #region Private methods
+
+        #endregion
     }
 }
