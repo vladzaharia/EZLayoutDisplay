cf9aff44456bfead034ac1a3881d43012739e109
Invvard <7305493+Invvard@users.noreply.github.com>
2019-08-01 21:58:58 -0400

    Create Planck EZ classes

---
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards/PlanckEzKeyboard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards.ViewModel;
+using InvvardDev.EZLayoutDisplay.PluginContract;
+using InvvardDev.EZLayoutDisplay.PluginContract.Model;
+
+namespace InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards
+{
+    [Export(typeof(IKeyboardContract))]
+    public class PlanckEzKeyboard : ZsaKeyboardBase
+    {
+        public PlanckEzKeyboard()
+        {
+            LayoutDefinitionPath = "/data/planckEzLayoutDefinition.json";
+            SupportedKeyboardModel = new List<string> {
+                                                          "planck ez"
+                                                      };
+        }
+        protected override void CreateViewModel(IEnumerable<IEnumerable<KeyTemplate>> layoutTemplates)
+        {
+            _viewModel = new PlanckEzViewModel(layoutTemplates);
+        }
+
+        public override object GetKeyboardView()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards/ViewModel/PlanckEzViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using InvvardDev.EZLayoutDisplay.PluginContract.Model;
+
+namespace InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards.ViewModel
+{
+    public class PlanckEzViewModel : ZsaKeyboardViewModelBase
+    {
+        internal PlanckEzViewModel(IEnumerable<IEnumerable<KeyTemplate>> layoutTemplates) : base(layoutTemplates) { }
+    }
+}
