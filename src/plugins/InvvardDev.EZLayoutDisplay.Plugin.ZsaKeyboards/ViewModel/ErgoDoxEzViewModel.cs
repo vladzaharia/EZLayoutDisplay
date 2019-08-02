93b6f4579d33e7f1bcfade073d42be7def158dc6
Invvard <7305493+Invvard@users.noreply.github.com>
2019-08-01 21:50:31 -0400

    Move common code to base

--- src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards/ViewModel/ErgoDoxEzViewModel.cs
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards/ViewModel/ErgoDoxEzViewModel.cs
@@ -1,36 +1,12 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using GalaSoft.MvvmLight;
 using InvvardDev.EZLayoutDisplay.PluginContract.Model;
 
 namespace InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards.ViewModel
 {
-    public class ErgoDoxEzViewModel : ViewModelBase
+    public class ErgoDoxEzViewModel : ZsaKeyboardViewModelBase
     {
-        #region Fields
-
-        private IEnumerable<IEnumerable<KeyTemplate>> _layoutTemplates;
-        private ObservableCollection<KeyTemplate> _currentLayoutTemplate;
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Gets or sets the layout template.
-        /// </summary>
-        internal ObservableCollection<KeyTemplate> CurrentLayoutTemplate
+        public ErgoDoxEzViewModel(IEnumerable<IEnumerable<KeyTemplate>> layoutTemplates) : base(layoutTemplates)
         {
-            get => _currentLayoutTemplate;
-            set => Set(ref _currentLayoutTemplate, value);
-        }
-
-        #endregion
-
-        public ErgoDoxEzViewModel(IEnumerable<IEnumerable<KeyTemplate>> layoutTemplates)
-        {
-            _layoutTemplates = layoutTemplates;
-            CurrentLayoutTemplate = new ObservableCollection<KeyTemplate>();
         }
 
         #region Private methods
