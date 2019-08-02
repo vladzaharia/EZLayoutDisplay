b3d798f273c5635ecb340b8513648337a16d46e1
Invvard <7305493+Invvard@users.noreply.github.com>
2019-08-01 21:49:31 -0400

    Create ZsaKeyboardViewModelBase

---
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards/ViewModel/ZsaKeyboardViewModelBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+using InvvardDev.EZLayoutDisplay.PluginContract.Model;
+
+namespace InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards.ViewModel
+{
+    public class ZsaKeyboardViewModelBase : ViewModelBase
+    {
+        #region Fields
+
+        private IEnumerable<IEnumerable<KeyTemplate>> _layoutTemplates;
+        private ObservableCollection<KeyTemplate> _currentLayoutTemplate;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the layout template.
+        /// </summary>
+        private ObservableCollection<KeyTemplate> CurrentLayoutTemplate
+        {
+            get => _currentLayoutTemplate;
+            set => Set(ref _currentLayoutTemplate, value);
+        }
+
+        #endregion
+
+        protected ZsaKeyboardViewModelBase(IEnumerable<IEnumerable<KeyTemplate>> layoutTemplates)
+        {
+            _layoutTemplates = layoutTemplates;
+            CurrentLayoutTemplate = new ObservableCollection<KeyTemplate>();
+        }
+    }
+}
