3ddec3e3abbcc2a93031670fd14c331e84ca9751
Invvard <7305493+Invvard@users.noreply.github.com>
2019-08-01 21:50:06 -0400

    Create ZsaKeyboardBase class

---
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards/ZsaKeyboardBase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards.Service;
+using InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards.ViewModel;
+using InvvardDev.EZLayoutDisplay.PluginContract;
+using InvvardDev.EZLayoutDisplay.PluginContract.Model;
+
+namespace InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards
+{
+    public abstract class ZsaKeyboardBase : IKeyboardContract
+    {
+        protected string LayoutDefinitionPath;
+        protected readonly ILayoutService _layoutService;
+        protected ZsaKeyboardViewModelBase _viewModel;
+
+        public IEnumerable<string> SupportedKeyboardModel { get; protected set; }
+
+        protected ZsaKeyboardBase()
+        {
+            _layoutService = new LayoutService();
+        }
+
+        public string GetCurrentLayerName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task LoadLayoutAsync(EZLayout ezLayout)
+        {
+            var layoutDefinition = await _layoutService.LoadLayoutDefinitionAsync(LayoutDefinitionPath);
+            var layoutTemplates = await _layoutService.PopulateLayoutTemplatesAsync(layoutDefinition, ezLayout);
+
+            CreateViewModel(layoutTemplates);
+        }
+
+        protected abstract void CreateViewModel(IEnumerable<IEnumerable<KeyTemplate>> layoutTemplates);
+
+        public abstract object GetKeyboardView();
+
+    }
+}
