844d9fc3ba0910cb7835b6a6633176b30bd13486
Invvard <7305493+Invvard@users.noreply.github.com>
2019-08-11 11:59:59 -0400

    Implement the SwitchLayer metho and reformat

--- src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards/ZsaKeyboardBase.cs
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards/ZsaKeyboardBase.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards.Service;
 using InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards.ViewModel;
 using InvvardDev.EZLayoutDisplay.PluginContract;
+using InvvardDev.EZLayoutDisplay.PluginContract.Enum;
 using InvvardDev.EZLayoutDisplay.PluginContract.Model;
 
 namespace InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards
@@ -10,7 +12,7 @@ namespace InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards
     public abstract class ZsaKeyboardBase : IKeyboardContract
     {
         protected string LayoutDefinitionPath;
-        protected readonly ILayoutService _layoutService;
+        private readonly ILayoutService _layoutService;
         protected ZsaKeyboardViewModelBase _viewModel;
 
         public IEnumerable<string> SupportedKeyboardModel { get; protected set; }
@@ -20,22 +22,42 @@ namespace InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards
             _layoutService = new LayoutService();
         }
 
+        #region IKeyboardContract implementation
+
+        /// <inheritdoc />
         public string GetCurrentLayerName()
         {
             throw new System.NotImplementedException();
         }
 
+        /// <inheritdoc />
         public async Task LoadLayoutAsync(EZLayout ezLayout)
         {
             var layoutDefinition = await _layoutService.LoadLayoutDefinitionAsync(LayoutDefinitionPath);
-            var layoutTemplates = await _layoutService.PopulateLayoutTemplatesAsync(layoutDefinition, ezLayout);
+            var layoutTemplates = await _layoutService.PopulateLayoutTemplatesAsync(layoutDefinition.ToList(), ezLayout);
 
             CreateViewModel(layoutTemplates);
         }
 
-        protected abstract void CreateViewModel(IEnumerable<IEnumerable<KeyTemplate>> layoutTemplates);
+        /// <inheritdoc />
+        public void SwitchLayer(SwitchDirection direction)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        /// <inheritdoc />
         public abstract object GetKeyboardView();
 
+        #endregion
+
+        #region Private methods
+        
+        /// <summary>
+        /// Creates the view model to be used alongside the view.
+        /// </summary>
+        /// <param name="layoutTemplates">The <see cref="List{KeyTemplate}"/> to be displayed.</param>
+        protected abstract void CreateViewModel(IEnumerable<IEnumerable<KeyTemplate>> layoutTemplates);
+        
+        #endregion
     }
 }
