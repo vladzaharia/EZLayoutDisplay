0858bd951bcc387b9318120038b852e5cbf4a671
Invvard <7305493+Invvard@users.noreply.github.com>
2019-08-11 11:18:32 -0400

    Create the SwitchDirection enum

---
+++ src/plugins/InvvardDev.EZLayoutDisplay.PluginContract/Enum/SwitchDirection.cs
@@ -0,0 +1,18 @@
+﻿namespace InvvardDev.EZLayoutDisplay.PluginContract.Enum
+{
+    /// <summary>
+    /// Enum that indicates the direction to switch to.
+    /// </summary>
+    public enum SwitchDirection
+    {
+        /// <summary>
+        /// Switch direction is going up.
+        /// </summary>
+        Up = 0,
+
+        /// <summary>
+        /// Switch direction is going down.
+        /// </summary>
+        Down = 1
+    }
+}
