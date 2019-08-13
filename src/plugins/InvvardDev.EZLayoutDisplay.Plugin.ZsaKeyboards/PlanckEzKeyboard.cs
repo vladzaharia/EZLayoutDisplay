f6c98a2ab2cefa96016d06d85fd82254e43a0f86
Invvard <7305493+Invvard@users.noreply.github.com>
2019-08-12 22:29:44 -0400

    Seal inherited class

--- src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards/PlanckEzKeyboard.cs
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards/PlanckEzKeyboard.cs
@@ -2,12 +2,11 @@
 using System.ComponentModel.Composition;
 using InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards.ViewModel;
 using InvvardDev.EZLayoutDisplay.PluginContract;
-using InvvardDev.EZLayoutDisplay.PluginContract.Model;
 
 namespace InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards
 {
-    [Export(typeof(IKeyboardContract))]
-    public class PlanckEzKeyboard : ZsaKeyboardBase
+    [ Export(typeof(IKeyboardContract)) ]
+    public sealed class PlanckEzKeyboard : ZsaKeyboardBase
     {
         public PlanckEzKeyboard()
         {
@@ -17,15 +16,11 @@ namespace InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards
                                                       };
         }
 
-        /// <inheritdoc />
-        protected override void CreateViewModel(IEnumerable<IEnumerable<KeyTemplate>> layoutTemplates)
-        {
-            _viewModel = new PlanckEzViewModel(layoutTemplates);
-        }
-
         public override object GetKeyboardView()
         {
-            throw new System.NotImplementedException();
+            ViewModel = new PlanckEzViewModel();
+
+            return KeyboardView;
         }
     }
 }
