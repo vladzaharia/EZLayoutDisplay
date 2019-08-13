b98a667f838c383f81dfd77c6f8a78a5cdd77d84
Invvard <7305493+Invvard@users.noreply.github.com>
2019-08-12 22:27:13 -0400

    Simplify inherited class

--- src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards/ViewModel/ErgoDoxEzViewModel.cs
+++ src/plugins/InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards/ViewModel/ErgoDoxEzViewModel.cs
@@ -1,12 +1,8 @@
-﻿using System.Collections.Generic;
-using InvvardDev.EZLayoutDisplay.PluginContract.Model;
-
-namespace InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards.ViewModel
+﻿namespace InvvardDev.EZLayoutDisplay.Plugin.ZsaKeyboards.ViewModel
 {
     internal class ErgoDoxEzViewModel : ZsaKeyboardViewModelBase
     {
-        internal ErgoDoxEzViewModel(IEnumerable<IEnumerable<KeyTemplate>> layoutTemplates) : base(layoutTemplates)
-        {
+        internal ErgoDoxEzViewModel() {
         }
     }
 }
