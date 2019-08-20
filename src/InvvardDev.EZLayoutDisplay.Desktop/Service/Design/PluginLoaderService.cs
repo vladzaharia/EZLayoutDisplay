bde42fadafaa82e6b787ef96575e9ae8ae153749
Invvard <7305493+Invvard@users.noreply.github.com>
2019-08-19 22:20:15 -0400

    Implement IPluginLoaderService

---
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Service/Design/PluginLoaderService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using InvvardDev.EZLayoutDisplay.Desktop.Service.Interface;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Service.Design
+{
+    public class PluginLoaderService<T> : IPluginLoader<T>
+    {
+        public IEnumerable<T> Plugins { get; set; }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Service/Implementation/PluginLoaderService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using InvvardDev.EZLayoutDisplay.Desktop.Service.Interface;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Service.Implementation
+{
+    public class PluginLoaderService<T> : IDisposable, IPluginLoader<T>
+    {
+        private readonly CompositionContainer _container;
+
+        [ImportMany]
+        public IEnumerable<T> Plugins { get; set; }
+
+        public PluginLoaderService(string path)
+        {
+            var catalog = new DirectoryCatalog(path);
+
+            _container = new CompositionContainer(catalog);
+            _container.ComposeParts(this);
+        }
+
+        public void Dispose()
+        {
+            _container.Dispose();
+        }
+    }
+}
