8fcbd0dc224a60ef28702ca8933018f973da9f50
Invvard <7305493+Invvard@users.noreply.github.com>
2020-12-28 20:50:12 -0500

    Add assembly reference

--- src/InvvardDev.EZLayoutDisplay.Keyboards.Common/Service/LayoutService.cs
+++ src/InvvardDev.EZLayoutDisplay.Keyboards.Common/Service/LayoutService.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using InvvardDev.EZLayoutDisplay.Keyboards.Common.Model;
+using Newtonsoft.Json;
 
 namespace InvvardDev.EZLayoutDisplay.Keyboards.Common.Service
 {
