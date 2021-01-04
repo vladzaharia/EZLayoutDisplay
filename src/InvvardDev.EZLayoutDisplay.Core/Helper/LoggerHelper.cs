8d911f521cb02664f8b180a2b072f7f5a445ffc6
Invvard <7305493+Invvard@users.noreply.github.com>
2021-01-03 21:29:41 -0500

    Fix typo

--- src/InvvardDev.EZLayoutDisplay.Core/Helper/LoggerHelper.cs
+++ src/InvvardDev.EZLayoutDisplay.Core/Helper/LoggerHelper.cs
@@ -14,7 +14,7 @@ namespace InvvardDev.EZLayoutDisplay.Core.Helper
         }
 
         internal static void TraceCommand(this Logger                 logger,
-                                          string                      message          = "[Relay Command] {0} (line {1})",
+                                          string                      message          = "[Command] {0} (line {1})",
                                           [ CallerMemberName ] string memberName       = "",
                                           [ CallerLineNumber ] int    sourceLineNumber = 0)
         {
